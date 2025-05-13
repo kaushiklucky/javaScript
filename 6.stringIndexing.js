1b6a44f375f52803edc1308585882eff4284abdb
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-13 16:44:19 +0530

    first commit

---
+++ 6.stringIndexing.js
@@ -0,0 +1,20 @@
+// String indexing 
+
+let firstName = "LuckyKaushik";
+
+//  L    u   c   k   y   k   a   u   s   h   i    k
+//  0    1   2   3   4   5   6   7   8   9   10   11
+
+console.log(firstName[0]);
+// length of string 
+// firstName.length 
+
+console.log(firstName.length);
+
+// last character
+// last Index : length - 1 
+console.log(firstName[firstName.length-1]);
+
+// space will also be include in indexing
+let a = "Lucky  UCA   ";
+console.log(a.length);
