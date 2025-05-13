bb110da381cc44a2335452ff84f6de1d789b8b1a
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-13 16:42:54 +0530

    first commit

---
+++ 4.letDeclarationAndConst.js
@@ -0,0 +1,15 @@
+// let keyword 
+// declare variable with let keyword 
+
+let firstName = "Lucky";
+firstName = "Lucy";
+console.log(firstName);
+
+
+// block scope vs fu ntion scope 
+
+
+// declare constants 
+
+const pi = 3.14;
+console.log(pi);
