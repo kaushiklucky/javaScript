c7f9f505699b12b87c53f161926e36cf7d8e1807
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-14 15:04:12 +0530

    first commit

---
+++ 10.templetString.js
@@ -0,0 +1,16 @@
+//templet string
+
+let string1 = "20";
+let string2 = "10";
+
+let first = "Lucky";
+let second = "Kaushik";
+
+
+// My name is Lucky Kaushik, my age is 20 years
+let aboutMe = "My name is " + first + " " + second + ", my age is " + string1 + " years";
+console.log(aboutMe);
+
+console.log("Second methord");
+let aboutMe2 = `My name is ${first} ${second}, my age is ${string1} years`;
+console.log(aboutMe2);
