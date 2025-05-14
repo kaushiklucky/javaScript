a7895213202276c933911934aac4ccf7f2734acf
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-14 15:03:38 +0530

    first commit

---
+++ 8.typesOfOperaters.js
@@ -0,0 +1,31 @@
+// typeof operator 
+
+// data types (primitive data types)
+// string "Lucky"
+// number 2, 4, 5.6 
+// booleans 
+// undefined
+// null 
+// BigInt
+// Symbol
+
+let age = 22; 
+let firstName = "Lucky";
+
+console.log(typeof age);
+console.log(typeof firstName);
+
+// convert number to string. 
+// 22 -> "22"
+age = age + "";
+console.log(typeof age ); //"22"
+
+
+// convert string to number. 
+
+let myStr = +"34";
+console.log(typeof myStr);
+
+// let age = "18";
+// age = Number(age);
+// console.log(typeof age);
