7718ca41370a6979e1b0232cf2af29d7d1d39d51
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-14 15:04:31 +0530

    first commit

---
+++ 12.boolComparisionOperator.js
@@ -0,0 +1,21 @@
+// booleans & comparison operator 
+
+// booleans 
+// true, false 
+
+let num1 = 6;
+let num2 = "7";
+
+// console.log(num1<num2);
+
+// == vs === 
+// == -> chekes numbers not data type in javascript
+// === -> chekes data type also
+
+// console.log(num1 === num2);
+
+// != vs !==
+// != -> chekes data type 
+// !== -> chekes numbers not data type in javascript
+console.log(num1 != num2);
+console.log(num1 !== num2);
