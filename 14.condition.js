5a7bd43cee384d9c59780c5f5dcb4bd942308d3c
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-15 16:36:10 +0530

    first commit

---
+++ 14.condition.js
@@ -0,0 +1,58 @@
+// if else condition 
+
+// let age = 17;
+
+// if(age>=18){
+//     console.log("User can play ddlc");
+// }else {
+//     console.log("User can play mario");
+// }
+
+// let num = 13;
+
+// if(num%2===0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+// falsy values 
+
+
+// false
+// ""
+// null 
+// undefined
+// 0
+
+// truthy 
+// "abc"
+// 1, -1
+
+// let firstName= 0;
+
+// if(firstName){
+//     console.log(firstName);
+// }else{
+//     console.log("firstName is kinda empty");
+// }
+
+
+// ternary operator 
+
+// let age = 4;
+// let drink;
+
+// if(age>=5){
+//     drink = "coffee";
+// }else{
+//     drink = "milk";
+// }
+
+// console.log(drink);
+
+// ternary operator / conditional operator 
+
+// let age = 3;
+// let drink = age >= 5 ? "coffee" : "milk";
+// console.log(drink);
