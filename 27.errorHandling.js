6c94f9fea86b80aa58daf490a5238863ff8ba175
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-21 23:50:34 +0530

    first commit

---
+++ 27.errorHandling.js
@@ -0,0 +1,27 @@
+//compile time error
+// console.log(1;  // syntax error
+
+// runtime error 
+// console.log(x); // reference error
+
+try{
+    console.log("try block start here");
+    console.log(x);
+    console.log("try block ends here");
+
+    // code
+    // code
+}
+catch(error){
+    //define krte h, error k sath aap kya karna chahte hai
+    // retry logic
+    // fallback mechanism
+    // logging
+    // custom error
+
+    console.log("I AM INSIDE CATCH BLOCK");
+    console.log("Your error is here: ", error);
+}
+finally{
+    console.log("I will run everytime, as I am finally block")
+}
