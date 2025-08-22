ed88011af4c38117a773484011c8984438eae54d
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-22 14:48:45 +0530

    updated 1.0

--- 14.condition.js
+++ 14.condition.js
@@ -1,20 +1,20 @@
 // if else condition 
 
-// let age = 17;
+let age = 17;
 
-// if(age>=18){
-//     console.log("User can play ddlc");
-// }else {
-//     console.log("User can play mario");
-// }
+if(age>=18){
+    console.log("User can play ddlc");
+}else {
+    console.log("User can play mario");
+}
 
-// let num = 13;
+let num = 13;
 
-// if(num%2===0){
-//     console.log("even");
-// }else{
-//     console.log("odd");
-// }
+if(num%2===0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
 
 // falsy values 
 
@@ -29,30 +29,41 @@
 // "abc"
 // 1, -1
 
-// let firstName= 0;
+let firstName= 0;
 
-// if(firstName){
-//     console.log(firstName);
-// }else{
-//     console.log("firstName is kinda empty");
-// }
+if(firstName){
+    console.log(firstName);
+}else{
+    console.log("firstName is kinda empty");
+}
 
 
 // ternary operator 
 
-// let age = 4;
-// let drink;
+age = 4;
+let drink;
 
-// if(age>=5){
-//     drink = "coffee";
-// }else{
-//     drink = "milk";
-// }
+if(age>=5){
+    drink = "coffee";
+}else{
+    drink = "milk";
+}
 
-// console.log(drink);
+console.log(drink);
 
 // ternary operator / conditional operator 
 
-// let age = 3;
-// let drink = age >= 5 ? "coffee" : "milk";
-// console.log(drink);
+age = 3;
+drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
+
+
+let a = "Hey, what's your age?";
+a = Number.parseInt(a); // converting string to number
+console.log(a);
+
+if (a > 0) {
+    console.log("This is a valid age");
+} else {
+    console.log("This is not a valid age");
+}
