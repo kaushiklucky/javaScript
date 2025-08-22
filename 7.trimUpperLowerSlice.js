a0471a2b922da2bd33345a81ec0da744e62e95ce
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-22 14:48:05 +0530

    updated 1.0

--- 7.trimUpperLowerSlice.js
+++ 7.trimUpperLowerSlice.js
@@ -19,8 +19,16 @@ console.log(firstName);
 // start index 
 // end index
 
-let newString = firstName.slice(0, 5); // hars
+let newString = firstName.slice(0, 5); // "lucky"
 console.log(newString);
 
 let secondString = second.slice(6, 14);
 console.log(secondString);
+
+let str = "Please give rs 1000";
+let amount = str.slice("Please give rs ".length);
+console.log(amount); // "1000"
+
+console.log(typeof(amount)); // string
+amount = Number.parseInt(amount);
+console.log(typeof(amount));
